26a9cd727c21d529a5979e0d7c223415d47e7370
bysking <1982287730@qq.com>
2020-04-25 23:51:24 +0800

    [add] 新增立方体创建和渲染循环

--- three-js-learning/three-js-demo/index.js
+++ three-js-learning/three-js-demo/index.js
@@ -18,5 +18,30 @@ let renderer = new THREE.WebGLRenderer();
  * param 3 false 低分辨率渲染， 需要配合wwidth/2, wheight/2, 类似于用小图进行放大
  */
 renderer.setSize(wwidth, wheight, false);
-
+addGeometry(sceen);
+camera.position.z = 5;
 document.body.appendChild(renderer.domElement);
+
+animate();
+
+/**
+ * 
+ * @param {any} seen 场景实例
+ * @param {Object} options 创建立方体配置
+ */
+function addGeometry(seen, options) {
+    let geometry = new THREE.BoxGeometry(1, 1, 1); // 立方体对象， 顶点信息， 空间坐标
+    let materail = new THREE.MeshBasicMaterial({ color: '#00ff00' }); // 材料信息
+    let cube = new THREE.Mesh(geometry, materail); // 网格蒙皮
+    seen.add(cube); // 默认情况 物体会被添加到（0，0，0），使得摄像机和立方体在一起，解决方案： 摄像机外移
+}
+
+/**
+ * 渲染循环 每次屏幕刷新时对场景进行绘制的循环-大多数屏幕60次/秒
+ * 为什么不用setInterval替代？ 答： 用户切换其他标签页时requestAnimationFrame会暂停，节省处理器资源，省电
+ */
+function animate() {
+    requestAnimationFrame(animate);
+    // 自定义操作
+    renderer.render(sceen, camera)
+}
--- three-js-learning/threeJs.md
+++ three-js-learning/threeJs.md
@@ -66,6 +66,33 @@ renderer.setSize(wwidth, wheight, false);
 document.body.appendChild(renderer.domElement);
 ```
 
+- 创建立方体 放在document.body.appendChild(renderer.domElement);前面
+```javascript
+/**
+ * 
+ * @param {any} seen 场景实例
+ * @param {Object} options 创建立方体配置
+ */
+function addGeometry(seen, options) {
+    let geometry = new THREE.BoxGeometry(1, 1, 1); // 立方体对象， 顶点信息， 空间坐标
+    let materail = new THREE.MeshBasicMaterial({ color: '#00ff00' }); // 材料信息
+    let cube = new THREE.Mesh(geometry, materail); // 网格蒙皮
+    seen.add(cube); // 默认情况 物体会被添加到（0，0，0），使得摄像机和立方体在一起，解决方案： 摄像机外移
+}
+```
+- 添加渲染循环
+```javascript
+/**
+ * 渲染循环 每次屏幕刷新时对场景进行绘制的循环-大多数屏幕60次/秒
+ * 为什么不用setInterval替代？ 答： 用户切换其他标签页时requestAnimationFrame会暂停，节省处理器资源，省电
+ */
+function animate() {
+    requestAnimationFrame(animate);
+    // 自定义操作
+    renderer.render(sceen, camera)
+}
+```
+
 > 通过模块引入
 
 > 浏览器支持
